8856b8cb984e02fdb2ae426d4415ba2f6b971d6b
parksooo <popomarin28@gmail.com>
2024-07-12 16:53:57 +0900

    [fix] : delete "public" keyword

--- src/main/java/com/match_management/demo/stat/StatRepository.java
+++ src/main/java/com/match_management/demo/stat/StatRepository.java
@@ -4,5 +4,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatRepository extends JpaRepository<Stat, Long> {
-    public Optional<Stat> findByUserId(final Long userId);
+    Optional<Stat> findByUserId(final Long userId);
 }
