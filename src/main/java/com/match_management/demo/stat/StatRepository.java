7e587465eb6ec690365f287960e4f3a0d6ee310a
parksooo <popomarin28@gmail.com>
2024-07-12 21:30:03 +0900

    [feat] : add Repository Annotation

--- src/main/java/com/match_management/demo/stat/StatRepository.java
+++ src/main/java/com/match_management/demo/stat/StatRepository.java
@@ -2,7 +2,9 @@ package com.match_management.demo.stat;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StatRepository extends JpaRepository<Stat, Long> {
     Optional<Stat> findByUserId(final Long userId);
 }
