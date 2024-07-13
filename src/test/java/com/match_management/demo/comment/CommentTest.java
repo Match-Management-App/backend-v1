be5316a38f017036315c5154f42a9d12742e1f9a
parksooo <popomarin28@gmail.com>
2024-07-13 17:22:19 +0900

    [feat] : set initUser

--- src/test/java/com/match_management/demo/comment/CommentTest.java
+++ src/test/java/com/match_management/demo/comment/CommentTest.java
@@ -1,8 +1,10 @@
 package com.match_management.demo.comment;
 
-import com.match_management.demo.board.Board;
-import com.match_management.demo.board.BoardRepository;
 import com.match_management.demo.board.BoardService;
+import com.match_management.demo.user.User;
+import com.match_management.demo.user.UserRepository;
+import com.match_management.demo.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +24,23 @@ public class CommentTest {
     CommentService commentService;
     @Autowired
     CommentRepository commentRepository;
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
+    private static Long USER_ID;
+    private static Long BOARD_ID;
+    @BeforeEach
+    public void initUserAndBoard() {
+        String name = "suhwpark";
+        String position = "middleFielder";
+
+        USER_ID = userService.create(name, position);
+        User user = userRepository.findById(USER_ID).orElseThrow(RuntimeException::new);
+
+        //when
+        BOARD_ID = boardService.create(user.getId(), user.getName(), "내일 경기 투표");
+    }
 
     //게시글에 댓글 달기
     @Test
@@ -30,10 +49,11 @@ public class CommentTest {
         String text = "용병 1";
 
         //when
-        Long commentId = commentService.create(userId, boardId, text);
+        Long commentId = commentService.create(USER_ID, BOARD_ID, text);
 
         //then
-        Comment comment = commentRepository.findById(commentId);
-        assertThat(commnet.getText()).isEqualTo(text);
+        Comment comment = commentRepository.findByBoardId(commentId)
+                .orElseThrow(RuntimeException::new);
+        assertThat(comment.getText()).isEqualTo(text);
     }
 }
