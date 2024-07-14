76ddcc8c6d6973e2d49a96772bc5d99608b96923
parksooo <popomarin28@gmail.com>
2024-07-14 16:21:16 +0900

    [test] : get voted name

--- src/test/java/com/match_management/demo/vote/VoteTest.java
+++ src/test/java/com/match_management/demo/vote/VoteTest.java
@@ -45,7 +45,7 @@ public class VoteTest {
 
     // 한명이 참석으로 투표했을 경우의 test
     @Test
-    public void create() {
+    public void viewVoteResult() {
         //given
         voteService.create(USER_ID, BOARD_ID, true);
 
@@ -55,5 +55,45 @@ public class VoteTest {
 
         //then
         assertThat(voteResult.get(0)).isEqualTo(1);
+    }
+
+    //한명도 투표하지 않았을 경우
+    @Test
+    public void viewResultNoOneVote() {
+        //when
+        List<Integer> voteResult = voteService.result(BOARD_ID);
+
+        //then
+        assertThat(voteResult.get(0)).isEqualTo(0);
+        assertThat(voteResult.get(1)).isEqualTo(0);
+    }
+
+    //투표한 사람의 명단
+    @Test
+    public void viewNameWhoVote() {
+        //given
+        Long user1 = userService.create("su", "forwoard");
+        Long user2 = userService.create("suhwpark", "forwoard");
+        Long user3 = userService.create("say", "forwoard");
+        Long user4 = userService.create("niu", "forwoard");
+        Long user5 = userService.create("yong", "forwoard");
+
+        voteService.create(USER_ID, BOARD_ID, true);
+        voteService.create(user1, BOARD_ID, true);
+        voteService.create(user2, BOARD_ID, true);
+        voteService.create(user3, BOARD_ID, false);
+        voteService.create(user4, BOARD_ID, true);
+        voteService.create(user5, BOARD_ID, false);
+
+        //when
+        List<String> result = voteService.votedName(BOARD_ID);
+
+        //then
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.contains("su")).isTrue();
+        assertThat(result.contains("suhwpark")).isTrue();
+        assertThat(result.contains("niu")).isTrue();
+        assertThat(result.contains("say")).isFalse();
+        assertThat(result.contains("yong")).isFalse();
     }
 }
