128fc35dbee1eaea5193bbdaee1ff62220b2aa54
parksooo <popomarin28@gmail.com>
2024-07-21 19:28:23 +0900

    [feat] : view user Best assist user about user own goal

---
+++ src/main/java/com/match_management/demo/chemicalPartner/ChemicalPartnerService.java
@@ -0,0 +1,46 @@
+package com.match_management.demo.chemicalPartner;
+
+import com.match_management.demo.user.User;
+import com.match_management.demo.user.UserRepository;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChemicalPartnerService {
+    private final ChemicalPartnerRepository chemicalPartnerRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long create(final Long goalMemberId, final Long assistMemberId,
+                       final int stat, final LocalDate matchDate) {
+        final ChemicalPartner chemicalPartner = chemicalPartnerRepository
+                .findByGoalMemberIdAndAssistMemberId(goalMemberId, assistMemberId)
+                        .orElse(new ChemicalPartner(goalMemberId, assistMemberId, 0L, matchDate));
+
+        chemicalPartner.addStat(stat);
+        chemicalPartnerRepository.save(chemicalPartner);
+
+        return chemicalPartner.getId();
+    }
+
+    public List<String> getAssistPartner(final Long userId) {
+        List<ChemicalPartner> chemicalPartners = chemicalPartnerRepository
+                .findByGoalMemberIdAndOrderByStat(userId, PageRequest.of(0, 3));
+
+        return chemicalPartners
+                .stream()
+                .map(c -> {
+                    User user = userRepository
+                            .findById(c.getAssistMemberId())
+                            .orElseThrow(RuntimeException::new);
+                    return user.getName();
+                })
+                .toList();
+    }
+}
