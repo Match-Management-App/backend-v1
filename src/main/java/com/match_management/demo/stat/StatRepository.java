f2a5805623b49eb224fb5f71e9dc69c9922279d1
parksooo <popomarin28@gmail.com>
2024-07-24 16:57:55 +0900

    [feat] : top 3 member about stats

--- src/main/java/com/match_management/demo/stat/StatRepository.java
+++ src/main/java/com/match_management/demo/stat/StatRepository.java
@@ -1,10 +1,29 @@
 package com.match_management.demo.stat;
 
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StatRepository extends JpaRepository<Stat, Long> {
     Optional<Stat> findByUserId(final Long userId);
+
+    @Query("select s from Stat s "
+            + "ORDER BY s.goalPoints DESC")
+    List<Stat> findByGoalPointsDESC(final Pageable pageable);
+
+    @Query("select s from Stat s "
+            + "ORDER BY s.assistPoints DESC")
+    List<Stat> findByAssistPointsDESC(final Pageable pageable);
+
+    @Query("select s from Stat s "
+            + "ORDER BY s.defencePoints DESC")
+    List<Stat> findByDefencePointsDESC(final Pageable pageable);
+
+    @Query("select s from Stat s "
+            + "ORDER BY s.attendancePoints DESC")
+    List<Stat> findByAttendancePointsDESC(final Pageable pageable);
 }
--- src/main/java/com/match_management/demo/stat/StatService.java
+++ src/main/java/com/match_management/demo/stat/StatService.java
@@ -1,14 +1,20 @@
 package com.match_management.demo.stat;
 
+import com.match_management.demo.user.User;
+import com.match_management.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class StatService {
     final private StatRepository statRepository;
+    final private UserRepository userRepository;
 
     @Transactional
     public Long initStat(final Long userId) {
@@ -52,5 +58,52 @@ public class StatService {
                 .orElseThrow(RuntimeException::new);
 
         stat.accumulateDefencePoints(defencePoints);
+    }
+
+    public List<String> getTop3GoalMembers() {
+        final List<Stat> stats = statRepository.findByGoalPointsDESC(PageRequest.of(0, 3));
+
+        return stats.stream()
+                .map(s -> {
+                    final User user = userRepository.findById(s.getUserId())
+                            .orElseThrow(RuntimeException::new);
+                    return user.getName();
+                })
+                .toList();
+    }
+
+    public List<String> getTopAssistMembers() {
+        final List<Stat> stats = statRepository.findByAssistPointsDESC(PageRequest.of(0, 3));
+
+        return stats.stream()
+                .map(s -> {
+                    final User user = userRepository.findById(s.getUserId())
+                            .orElseThrow(RuntimeException::new);
+                    return user.getName();
+                })
+                .toList();
+    }
+
+    public List<String> getTopDefenceMembers() {
+        final List<Stat> stats = statRepository.findByDefencePointsDESC(PageRequest.of(0, 3));
+
+        return stats.stream()
+                .map(s -> {
+                    final User user = userRepository.findById(s.getUserId())
+                            .orElseThrow(RuntimeException::new);
+                    return user.getName();
+                })
+                .toList();
+    }
+    public List<String> getTopAttendanceMembers() {
+        final List<Stat> stats = statRepository.findByAttendancePointsDESC(PageRequest.of(0, 3));
+
+        return stats.stream()
+                .map(s -> {
+                    final User user = userRepository.findById(s.getUserId())
+                            .orElseThrow(RuntimeException::new);
+                    return user.getName();
+                })
+                .toList();
     }
 }
