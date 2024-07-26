0a1871587a0d400f497a4d79ba595bcc6cba3d45
parksooo <popomarin28@gmail.com>
2024-07-26 16:27:45 +0900

    [fix] : package name

--- src/test/java/com/match_management/demo/stat/StatEntityTest.java
+++ src/test/java/com/match_management/demo/stat/StatEntityTest.java
@@ -1,7 +1,5 @@
-package com.match_management.demo.statTest;
+package com.match_management.demo.stat;
 
-import com.match_management.demo.stat.Stat;
-import com.match_management.demo.stat.StatService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
