384577cc756cd3b06a291e53296e090f0aeac825
parksooo <popomarin28@gmail.com>
2024-07-27 16:16:45 +0900

    [fix] : error in method name

--- src/main/java/com/match_management/demo/user/UserRepository.java
+++ src/main/java/com/match_management/demo/user/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(final Long id);
-    Optional<User> finaByOauthId(final Long oauthId);
+    Optional<User> findByOauthId(final Long oauthId);
 }
